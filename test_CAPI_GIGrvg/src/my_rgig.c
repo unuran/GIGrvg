bef7a6262c9f0bfd4b829171910bdfa3db653a91
jleydold <josef.leydold@wu.ac.at>
2023-04-29 18:19:54 +0200

    Added missing 'static' keyword in test package

--- test_CAPI_GIGrvg/src/my_rgig.c
+++ test_CAPI_GIGrvg/src/my_rgig.c
@@ -4,7 +4,7 @@
 
 SEXP my_rgig (SEXP n, SEXP lambda, SEXP chi, SEXP psi)
 { 
-   SEXP (*fun)(SEXP,SEXP,SEXP,SEXP) = NULL;
+   static SEXP (*fun)(SEXP,SEXP,SEXP,SEXP) = NULL;
 
    if (!fun) 
       fun = (SEXP(*)(SEXP,SEXP,SEXP,SEXP)) R_GetCCallable("GIGrvg", "rgig");
